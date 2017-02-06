3f6c53f326b3c74dda3492910fd0808aea378a69
Fernanda <fernandamora88@gmail.com>
2017-02-05 19:48:13 -0800

    add dealer to blackjack

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -10,14 +10,22 @@ class Dealer < Player
   end
 
   def place_bet(dealer, amt)
+    raise "Dealer doesn't bet"
   end
 
   def play_hand(deck)
+    while @hand.points < 17
+      @hand.hit(deck)
+    end
   end
 
   def take_bet(player, amt)
+    @bets[player] = amt
   end
 
   def pay_bets
+    @bets.each do |player, bet|
+      player.pay_winnings(2*bet) if player.hand.beats?(@hand)
+    end
   end
 end
