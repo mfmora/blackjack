f5cbff02753c19954bcc9e4bf6bca6a6611be297
Fernanda <fernandamora88@gmail.com>
2017-02-05 19:39:53 -0800

    add hand to blackjack

--- lib/hand.rb
+++ lib/hand.rb
@@ -4,26 +4,49 @@ class Hand
   # object. This is in contrast to the `#initialize` method that
   # expects an `Array` of cards to hold.
   def self.deal_from(deck)
+    Hand.new(deck.take(2))
   end
 
   attr_accessor :cards
 
   def initialize(cards)
+    @cards = cards
   end
 
   def points
+    total_points = 0
+    aces_count = 0
+    @cards.each do |card|
+      if card.value == :ace
+        aces_count += 1
+        total_points += 11
+      else
+        total_points += card.blackjack_value
+      end
+    end
+    aces_count.times do
+      total_points -= 10 if total_points > 21
+    end
+    total_points
   end
 
   def busted?
+    points > 21
   end
 
   def hit(deck)
+    raise "already busted" if busted?
+    @cards += deck.take(1)
   end
 
   def beats?(other_hand)
+    return false if busted?
+    points > other_hand.points || other_hand.busted?
   end
 
   def return_cards(deck)
+    deck.return(@cards)
+    @cards = []
   end
 
   def to_s
