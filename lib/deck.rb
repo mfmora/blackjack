da33ef609084bfcfb4a76e00accd617e0a9cd404
Fernanda <fernandamora88@gmail.com>
2017-02-05 19:31:15 -0800

    add deck functionality

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,20 +4,38 @@ require_relative 'card'
 class Deck
   # Returns an array of all 52 playing cards.
   def self.all_cards
+    result = []
+    Card.values.each do |value|
+      Card.suits.each do |suit|
+        result << Card.new(suit,value)
+      end
+    end
+    result
   end
 
   def initialize(cards = Deck.all_cards)
+    @cards = cards
   end
 
   # Returns the number of cards in the deck.
   def count
+    @cards.count
   end
 
   # Takes `n` cards from the top of the deck.
   def take(n)
+    raise "not enough cards" if n > count
+    result = []
+    n.times do
+      result << @cards.shift
+    end
+    result
   end
 
   # Returns an array of cards to the bottom of the deck.
   def return(cards)
+    cards.each do |card|
+      @cards << card
+    end
   end
 end
